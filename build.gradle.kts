55601c8f73d54623fdcf8e1567c0af3665280d6e
Marcel <mstolin+github@posteo.me>
2025-08-24 13:39:36 +0200

    Initialized project

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.android.lint) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.protobuf.plugin) apply false
+}
